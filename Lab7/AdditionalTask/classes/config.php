b30806b73dc1db1d3777aeea725aff1c69902224
Ilya <ilya1099@mail.ru>
2023-05-16 09:49:52 +0200

    Update

--- Lab7/AdditionalTask/classes/config.php
+++ Lab7/AdditionalTask/classes/config.php
@@ -14,9 +14,9 @@ $db['name'] = "name";
 // ------------------------
 
 $auth_conf['site_name'] = "Доставка пиццы"; // Name of website to appear in emails
-$auth_conf['email_from'] = "webmaster@holanto.com"; // Email FROM address for Auth emails (Activation, password reset...)
+$auth_conf['email_from'] = "test@test.com"; // Email FROM address for Auth emails (Activation, password reset...)
 $auth_conf['max_attempts'] = 5; // INT : Max number of attempts for login before user is locked out
-$auth_conf['base_url'] = "https://holanto.com/"; // URL to Auth Class installation root WITH trailing slash
+$auth_conf['base_url'] = "https://google.com/"; // URL to Auth Class installation root WITH trailing slash
 $auth_conf['session_duration'] = "+1 month"; // Amount of time session lasts for. Only modify if you know what you are doing ! Default = +1 month
 $auth_conf['security_duration'] = "+15 minutes"; // Amount of time to lock a user out of Auth Class afetr defined number of attempts.
 
